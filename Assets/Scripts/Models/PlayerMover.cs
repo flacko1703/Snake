0b4731a0bdbb372b1f37241ddb2528b620a6cce3
flacko1703 <51458578+flacko1703@users.noreply.github.com>
2021-10-06 10:40:41 +0300

    Slightly movement change

--- Assets/Scripts/Models/PlayerMover.cs
+++ Assets/Scripts/Models/PlayerMover.cs
@@ -5,6 +5,7 @@ namespace Snake
     public class PlayerMover : IMove
     {
         private float _moveSpeed = 10;
+        private float _sideMoveSpeed = 20;
         private Transform _instance;
         private CharacterController _characterController;
         private Vector2 _inputAxis;
@@ -25,7 +26,7 @@ namespace Snake
         {
             _inputAxis = new Vector2(Input.GetAxis("Horizontal"), 0);
             Vector3 desiredMove = _instance.right * _inputAxis.x + _instance.forward + _instance.up;
-            _moveDirection.x = desiredMove.x * _moveSpeed;
+            _moveDirection.x = desiredMove.x * _sideMoveSpeed;
             _moveDirection.z = desiredMove.y * _moveSpeed;
             _characterController.Move(_moveDirection * Time.deltaTime);
         }
